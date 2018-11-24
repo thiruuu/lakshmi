b666f4859921930933c7736753425355b7e1ba4c
thiruuu <45309534+thiruuu@users.noreply.github.com>
2018-11-24 16:16:16 +0530

    Update and rename README.md to basic1.java

---
+++ basic1.java
@@ -0,0 +1,17 @@
+# lakshmi
+/* package whatever; // don't place package name! */
+
+//import java.util.*;
+//import java.lang.*;
+//import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		System.out.println("Hai");
+	
+	}
+}
