1d00676ce6375046ca873b43633d45147e4a506f
thiruuu <45309534+thiruuu@users.noreply.github.com>
2018-11-29 15:56:40 +0530

    Create Myclass.java

---
+++ Myclass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       //int cnt=0;
+       int arr[]=new int[n];
+       for(int i=0;i<n;i++)
+       {
+           arr[i]=sc.nextInt();
+       }
+       if(n%2==0)
+       {
+           System.out.println("Yes");
+       }
+       else
+       {
+           System.out.println("No");
+       }
+    }
+}
