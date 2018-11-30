b36da7ec30f1dee7a0a2055ed626b33ff9541a2d
thiruuu <45309534+thiruuu@users.noreply.github.com>
2018-11-30 16:16:55 +0530

    Create arrayaddval.java

---
+++ arrayaddval.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class arrayaddval {
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+int sum = 0;
+int n = sc.nextInt();
+int q = sc.nextInt();
+int a[] = new int[n+1];
+int u[] = new int[q+1];
+int v[] = new int[q+1];
+for ( int i = 1 ; i <= n ; i++){
+a[i] = sc.nextInt();
+}
+for(int i = 1 ; i <= q ; i++){
+u[i] = sc.nextInt();
+v[i] = sc.nextInt();
+}
+for ( int i = 1 ; i <= q ; i++){
+sum = 0;
+for( int j = u[i]; j<=v[i] ; j++){
+sum = sum + a[j];
+}
+System.out.println(sum);
+}
+}
+}
