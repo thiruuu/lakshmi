c02c1f15676a1877ea661e7ca90ef8b8073a0b86
thiruuu <45309534+thiruuu@users.noreply.github.com>
2018-11-30 16:57:27 +0530

    Create tournament.java

---
+++ tournament.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class tournament {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	if(n%2==0)
+		System.out.println(0);
+	else
+		System.out.println(1);
+}
